61b2f1a5a6bac0ccc557744d96fa53fcaa7f69d0
Nick Maynard <nick.maynard@alumni.doc.ic.ac.uk>
2015-04-21 10:19:54 +0100

    Minify is-my-json-valid

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,11 +12,22 @@ module.exports = function (grunt) {
           }
         }
       }
+    },
+    uglify: {
+      'dist': {
+        options: {
+          sourceMap: true
+        },
+        files: {
+          'draft4/js/is-my-json-valid/is-my-json-valid.min.js': ['draft4/js/is-my-json-valid/is-my-json-valid.js']
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('default', ['browserify']);
+  grunt.registerTask('default', ['browserify', 'uglify']);
 
 };
