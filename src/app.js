2de792043ea9e4d077c9a447b9d40dd8b846ef21
Nick Maynard <nick.maynard@alumni.doc.ic.ac.uk>
2016-10-25 17:09:48 +0100

    Rip out Angulartics and use a basic hash change function instead

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 var angular = require('angular');
 
-angular.module('app', [require('angular-route'), 'angulartics', 'angulartics.google.analytics')]).config(function ($routeProvider) {
+angular.module('app', [require('angular-route')]).config(function ($routeProvider) {
 
   $routeProvider.when('/version/:specVersion/markup/:markupLanguage', {
     controller: "ValidatorController"
@@ -9,7 +9,12 @@ angular.module('app', [require('angular-route'), 'angulartics', 'angulartics.goo
     redirectTo: '/version/draft-05/markup/json'
   });
 
-});
+}).run(function ($rootScope, $location, $window) {
+  $rootScope.$on('$routeChangeSuccess', function () {
+    console.info("Hash change, informing GA");
+    $window.ga('send', 'pageview', $location.path());
+  });
+});;;
 
 require('./services');
 require('./controllers');
