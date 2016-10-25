f12560c10c3625344fa9f073313e6788612c488e
Nick Maynard <nick.maynard@alumni.doc.ic.ac.uk>
2016-10-25 17:11:02 +0100

    Update comment

--- www/ga.js
+++ www/ga.js
@@ -4,4 +4,4 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-30596645-1', 'auto');
-// ga('send', 'pageview'); // Angulartics does this for us
+// ga('send', 'pageview'); // We do this on hashchange
