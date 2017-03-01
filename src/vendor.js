50d5fff06f209294ea7d04ffbaafb9701cd6e28d
Nick Maynard <nick.maynard@alumni.doc.ic.ac.uk>
2017-03-01 15:08:06 +0000

    Move vendor dependencies into vendor bundle

--- src/vendor.js
+++ src/vendor.js
@@ -2,4 +2,7 @@
 require('bootstrap/dist/css/bootstrap.min.css');
 require('angular');
 require('angular-route');
+require('angular-sanitize');
+require('angular-translate');
+require('angular-translate-loader-static-files');
 require('angular-ui-bootstrap');
