500da4807c9639418871b5f6c1cc7959ebe58208
Nick Maynard <nickcmaynard@users.noreply.github.com>
2020-06-02 16:54:27 +0100

    Flip some "privacy" switches in GA.

--- www/ga.js
+++ www/ga.js
@@ -1,3 +1,6 @@
 window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
 ga('create', 'UA-30596645-1', 'auto');
+ga('set', 'anonymizeIp', true); // Anonymise IP
+ga('set', 'allowAdFeatures', false); // Disable ad features regardless of dashboard settings
+ga('set', 'forceSSL', true);
 // ga('send', 'pageview'); // We do this on hashchange
